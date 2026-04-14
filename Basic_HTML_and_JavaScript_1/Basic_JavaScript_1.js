22faa956422f13673439e565b7e59474889afc40
LeslySturgill <alonzol.25@outlook.com>
2026-04-14 16:17:28 -0700

    Basic_HTML_and_JavaScript_1
    Added my Basic_HTML_and_JavaScript_1 folder"

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World! I am an alert box!");
+function My_First_Function() {
+		var String = "Kiss me, I'm Mexican!";
+		var result = String.fontcolor("pink");
+		document.getElementById("Mexican").innerHTML=
+		result;
+	}
